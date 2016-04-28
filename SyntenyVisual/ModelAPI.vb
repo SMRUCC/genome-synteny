1b485f851631b2d94b6fa88d0b4d7e92b3242880
xieguigang <xie.guigang@gmail.com>
2016-04-29 00:11:00 +0800

    load mode data

--- SyntenyVisual/ModelAPI.vb
+++ SyntenyVisual/ModelAPI.vb
@@ -1,4 +1,23 @@
-﻿Public Module ModelAPI
+﻿Imports LANS.SystemsBiology.Assembly.NCBI.GenBank
+Imports LANS.SystemsBiology.Assembly.NCBI.GenBank.TabularFormat
+Imports LANS.SystemsBiology.NCBI.Extensions.Analysis
 
+Public Module ModelAPI
 
+    Public Function GetDrawsModel(path As String) As DrawingModel
+        Dim model As DeviceModel = Serialization.LoadJsonFile(Of DeviceModel)(path)
+        Dim DIR As New Directory(path.ParentPath)
+        Dim bbhMeta As BestHit = DIR.GetFullPath(model.Meta).LoadXml(Of BestHit)
+        Dim PTT As Dictionary(Of String, PTT) =
+            model.PTT.ToDictionary(Function(x) x.Key,
+                                   Function(x) TabularFormat.PTT.Load(x.Value))
+
+    End Function
 End Module
+
+Public Class DrawingModel
+
+    Public Property Links As Line()
+
+
+End Class
