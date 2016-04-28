49c60ef17ce414a2ce3b8b5285be58df9b87923c
xieguigang <xie.guigang@gmail.com>
2016-04-28 23:14:15 +0800

    Bézier porbably is still not works

--- SyntenyVisual/Ortholog.vb
+++ SyntenyVisual/Ortholog.vb
@@ -89,6 +89,37 @@ Public Class Polyline : Inherits Line
     End Sub
 End Class
 
+Public Class Bézier : Inherits Line
+
+    ''' <summary>
+    ''' 出现转折的长度的百分比
+    ''' </summary>
+    ''' <returns></returns>
+    Public ReadOnly Property Turnp As Double
+
+    ''' <summary>
+    ''' 在高度的多少百分比处开始转折？
+    ''' </summary>
+    ''' <param name="p"></param>
+    Sub New(from As Point, toPoint As Point, cl As Color, Optional p As Double = 0.2)
+        Call MyBase.New(from, toPoint, cl)
+        Turnp = p
+    End Sub
+
+    Public Overrides Sub Draw(ByRef gdi As GDIPlusDeviceHandle, width As Integer)
+        Dim height As Integer = [To].Y - From.Y       ' 由于假设To是下一个基因组，所以To的Y肯定会比From的Y的值要大
+        Dim w As Integer = Math.Abs(From.X - [To].X)  ' 但是水平的基因组上面的位置却不会一定是To.X要比From.X要大了
+        Dim ty As Integer = 2 * (height * Turnp)
+        Dim tx As Integer = 0.65 * (w * Turnp)
+        Dim order As Integer = If([To].X <= From.X, 1, -1) ' 正常的顺序
+        Dim t1 As New Point(From.X + order * tx, From.Y + ty)  ' 控制点 1
+        Dim t2 As New Point([To].X - order * tx, [To].Y - ty)  ' 控制点 2
+        Dim pen As New Pen(Color, width)
+
+        Call gdi.DrawBézier(pen, From, t1, t2, [To])
+    End Sub
+End Class
+
 ''' <summary>
 ''' 两个同源的基因之间的相连的线的样式
 ''' </summary>
@@ -104,7 +135,7 @@ Public Enum LineStyle
     ''' <summary>
     ''' 贝塞尔曲线
     ''' </summary>
-    Bezier
+    Bézier
 End Enum
 
 ''' <summary>
--- SyntenyVisual/Program.vb
+++ SyntenyVisual/Program.vb
@@ -8,6 +8,13 @@ Module Program
 
         Dim pol As New Polyline(New Point(100, 10), New Point(600, 120), Color.Red)
         Call pol.Draw(gdi, 5)
+
+        Dim bez As New Bézier(New Point(120, 10), New Point(200, 120), Color.Blue)
+        Call bez.Draw(gdi, 5)
+
+        bez = New Bézier(New Point(560, 10), New Point(200, 120), Color.Green)
+        Call bez.Draw(gdi, 5)
+
         Call gdi.Save("./testPol.png", ImageFormats.Png)
     End Sub
 End Module
