ca4e08b32aea39585deaab73864010d5611b52ee
xieguigang <xie.guigang@gmail.com>
2016-04-28 22:31:42 +0800

    basic drawing model

--- SyntenyVisual/Ortholog.vb
+++ SyntenyVisual/Ortholog.vb
@@ -1,3 +1,53 @@
-﻿Public Class Ortholog
+﻿Imports System.Drawing
+Imports Microsoft.VisualBasic.Imaging
+
+''' <summary>
+''' 直系同源的绘图模型
+''' </summary>
+Public MustInherit Class Line
+
+    Public ReadOnly Property From As Point
+    Public ReadOnly Property [To] As Point
+    Public ReadOnly Property Color As Color
+
+    Public MustOverride Sub Draw(ByRef gdi As GDIPlusDeviceHandle, width As Integer)
+
+End Class
+
+''' <summary>
+''' 
+''' </summary>
+''' <remarks>
+''' 这个绘图模型最简单
+''' </remarks>
+Public Class StraightLine : Inherits Line
+
+    Public Overrides Sub Draw(ByRef gdi As GDIPlusDeviceHandle, width As Integer)
+        Call gdi.DrawLine(New Pen(Color, width), From, [To])
+    End Sub
+End Class
+
+''' <summary>
+''' 两个同源的基因之间的相连的线的样式
+''' </summary>
+Public Enum LineStyle
+    ''' <summary>
+    ''' 直线
+    ''' </summary>
+    Straight
+    ''' <summary>
+    ''' 折线
+    ''' </summary>
+    Polyline
+    ''' <summary>
+    ''' 贝塞尔曲线
+    ''' </summary>
+    Bezier
+End Enum
+
+''' <summary>
+''' 直系同源的绘图数据模型
+''' </summary>
+Public Class Ortholog
 
 End Class
--- SyntenyVisual/Program.vb
+++ SyntenyVisual/Program.vb
@@ -1,6 +1,11 @@
-﻿Module Program
+﻿Imports System.Drawing
+Imports Microsoft.VisualBasic.Imaging
+
+Module Program
 
     Sub Main()
+        Dim gdi As GDIPlusDeviceHandle = New Size(1200, 800).CreateGDIDevice
+
 
     End Sub
 End Module
