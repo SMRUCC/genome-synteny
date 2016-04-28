520460f86c67dfb3d37939d93a5561df48ad9228
xieguigang <xie.guigang@gmail.com>
2016-04-28 22:44:05 +0800

    draw Polyline

--- SyntenyVisual/Ortholog.vb
+++ SyntenyVisual/Ortholog.vb
@@ -28,6 +28,53 @@ Public Class StraightLine : Inherits Line
 End Class
 
 ''' <summary>
+''' 
+''' </summary>
+''' <remarks>
+'''    +
+'''    |
+'''    |
+'''    \
+'''     \
+'''      \
+'''       \
+'''        \
+'''         \
+'''         |
+'''         |
+'''         +
+''' </remarks>
+Public Class Polyline : Inherits Line
+
+    ''' <summary>
+    ''' 出现转折的长度的百分比
+    ''' </summary>
+    ''' <returns></returns>
+    Public ReadOnly Property Turnp As Double
+
+    ''' <summary>
+    ''' 在高度的多少百分比处开始转折？
+    ''' </summary>
+    ''' <param name="p"></param>
+    Sub New(Optional p As Double = 0.2)
+        Turnp = p
+    End Sub
+
+    Public Overrides Sub Draw(ByRef gdi As GDIPlusDeviceHandle, width As Integer)
+        Dim height As Integer = [To].Y - From.Y
+        Dim t As Integer = height * Turnp
+        Dim t1 As New Point(From.X, From.Y + t)
+        Dim t2 As New Point([To].X, [To].Y - t)
+        Dim pen As New Pen(Color, width)
+
+        ' 绘图是从上到下进行的
+        Call gdi.DrawLine(pen, From, t1)
+        Call gdi.DrawLine(pen, t1, t2)
+        Call gdi.DrawLine(pen, t2, [To])
+    End Sub
+End Class
+
+''' <summary>
 ''' 两个同源的基因之间的相连的线的样式
 ''' </summary>
 Public Enum LineStyle
