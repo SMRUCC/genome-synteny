197adf93a909b94f5652b4c8236425332a584b50
xieguigang <xie.guigang@gmail.com>
2016-05-01 19:53:17 +0800

    add cli interface

--- SyntenyVisual/Program.vb
+++ SyntenyVisual/Program.vb
@@ -1,4 +1,5 @@
 ﻿Imports System.Drawing
+Imports Microsoft.VisualBasic.CommandLine
 Imports Microsoft.VisualBasic.DocumentFormat.Csv
 Imports Microsoft.VisualBasic.DocumentFormat.HTML
 Imports Microsoft.VisualBasic.Imaging
@@ -6,36 +7,18 @@ Imports Microsoft.VisualBasic.Serialization
 
 Module Program
 
-    Sub Main()
-
-        Dim html As String = "<font face=""Microsoft YaHei"" size=""5.5""><strong>text</strong><b> &lt;&lt;&lt; <i>value</i></b></font> "
-
-
-        Call New ClMap() {}.SaveTo("./colors.csv")
-
-        Dim strings = TextAPI.TryParse(html)
-        strings = TextAPI.TryParse("1234   56789    &lt;")
-
-        Dim gdi As GDIPlusDeviceHandle = New Size(1200, 800).CreateGDIDevice
-
-        Dim pol As New Polyline(New Point(100, 10), New Point(600, 120), Color.Red)
-        Call pol.Draw(gdi, 5)
-
-        Dim bez As New Bézier(New Point(120, 10), New Point(200, 120), Color.Blue)
-        Call bez.Draw(gdi, 5)
-
-        bez = New Bézier(New Point(560, 10), New Point(200, 120), Color.Green)
-        Call bez.Draw(gdi, 5)
-
-        Call gdi.Save("./testPol.png", ImageFormats.Png)
-
-
-        Dim model As DeviceModel = DeviceModel.Template
-        Call model.GetJson.SaveTo("./test.json")
-
+    Sub New()
         VBDebugger.Mute = True
-
-        Dim res = ModelAPI.GetDrawsModel("G:\Xanthomonas_campestris_8004_uid15\xcc.json").Visualize
-        Call res.Save("G:\Xanthomonas_campestris_8004_uid15\Xanthomonadales.png")
     End Sub
+
+    Public Function Main() As Integer
+        Return GetType(Program).RunCLI(App.CommandLine, executeFile:=AddressOf ExecuteFile)
+    End Function
+
+    Private Function ExecuteFile(file As String, args As CommandLine) As Integer
+        Dim model As DrawingModel = ModelAPI.GetDrawsModel(file)
+        Dim res As Drawing.Image = model.Visualize
+        Dim png As String = file.TrimFileExt & ".png"
+        Return res.SaveAs(png, ImageFormats.Png).CLICode
+    End Function
 End Module
