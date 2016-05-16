69b789b716102f45e68ac91ac378845f2de678ad
xieguigang <xie.guigang@gmail.com>
2016-05-16 19:13:51 +0800

    config for debugging

--- Synteny/Program.vb
+++ Synteny/Program.vb
@@ -14,6 +14,13 @@ Module Program
     End Sub
 
     Public Function Main() As Integer
+
+        Dim list As Double() = "G:\5.14.circos\03.ZIKV_45_2015_updated_mafft_named.GCSkew.txt".LoadDblVector
+        Dim img As New Bitmap(3000, 1000)
+        Dim res = LANS.SystemsBiology.GCModeller.DataVisualization.GCSkew.InvokeDrawingCurve(img, list, New Point(200, 850), New Size(2500, 800), LANS.SystemsBiology.GCModeller.DataVisualization.GraphicTypes.Curves)
+
+        Call res.SaveAs("x:\dddd.png", ImageFormats.Png)
+
         Return GetType(Program).RunCLI(App.CommandLine, executeFile:=AddressOf ExecuteFile)
     End Function
 
